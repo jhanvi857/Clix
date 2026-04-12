fdcaa015b2b4e2e45c1cbabc9f69d91877ae7842
jhanvi857 <jhanvip8507@gmail.com>
2026-04-12 19:44:57 +0530

    core : add shared types

---
+++ include/shell.h
@@ -0,0 +1,14 @@
+#pragma once
+#define MAX_INPUT    1024
+#define MAX_ARGS     64
+#define MAX_HISTORY  100
+#define SHELL_NAME   "mysh"
+
+typedef struct {
+    char  *args[MAX_ARGS];
+    int    argc;
+    char  *input_file;   
+    char  *output_file;  
+    int    append;       
+    int    background;   
+} Command;
